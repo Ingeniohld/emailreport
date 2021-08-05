2ce33df52413a8aa27718ae26125bd33d56a41a7
Ingenio <info@ingeniohld.com>
2021-08-05 11:18:03 +0000

    fix encoding error

--- standard_reports/models/.ipynb_checkpoints/models-checkpoint.py
+++ standard_reports/models/.ipynb_checkpoints/models-checkpoint.py
@@ -1,18 +1,34 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
+import xlrd
+import tempfile
+import binascii
+from odoo.exceptions import RedirectWarning, UserError, ValidationError
 
 class standard_reports(models.Model):
     _name = 'standard.reports'
     _description = 'standard_reports'
 
-    name = fields.Char()
+    name = fields.Char(compute='_get_data')
     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    
+    @api.depends('value')
+    def _get_data(self):
+        for rec in self :
+            document = self.env['documents.document'].search([
+                ('id', '=', 95)])
+            attach = self.env['ir.attachment'].search([('id','=',document.attachment_id.id)])
+            fp = tempfile.NamedTemporaryFile(suffix=".xlsx")
+            fp.write(binascii.a2b_base64(attach.datas))
+            fp.seek(0)
+            workbook = xlrd.open_workbook(fp.name)
+            sheet = workbook.sheet_by_index(0)
+            for row_no in range(sheet.nrows):
+                if row_no <= 0:
+                    fields = map(lambda row:row.value.encode('utf-8'), sheet.row(row_no))
+                else:
+                    line = list(map(lambda row:isinstance(row.value, str) and row.value.encode('utf-8') or str(row.value), sheet.row(row_no)))
+                    rec.name = line[0]
+#                     exit
+#                     raise UserError(line[1])
